88e5f8d72d6b6e86f68f518a297a14a10a07a938
DamjanStefanovski <Damjan.Stefanovski@gmail.com>
2018-05-04 11:35:46 -0400

    Ajdusting models and creating homepage templates

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+# Class
+
+class Blog(models.Model):
+    title = models.CharField(max_length=255)
+    pub_date = models.DateTimeField()
+    body = models.TextField()
+    image = models.ImageField(upload_to='images/')
+    
+
+    def __str__(self):
+        return self.title
+
+
+
+
+
+
+# Add to settings. > Create a migration. > migrate. > add to the admin view
+# Templates, > Staticfiles, Forms. and urls.py . acordingly
--- jobs/models.py
+++ jobs/models.py
@@ -1,7 +1,14 @@
 from django.db import models
-
 # Job descripting model representing past finished projects
 
 class Job(models.Model):
     image = models.ImageField(upload_to='images/',blank=True,null=True)
+    title = models.CharField(max_length=200)
     summary = models.CharField(max_length=200)
+
+    def __unicode__(self):  #this is a function/method and publish is the name of the method
+        self.title
+        self.save()
+#indent methods inside the class
+    def __str__(self):
+        return self.title
